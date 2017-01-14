ca7bac7c2d05fca4bdaf0c5279db3ca269e389b7
Akash Kothawale <akash@decached.com>
2017-01-14 03:37:00 -0500

    Accept IP:Port from cmd, CLI for `get "key"`
    - Take IP & Port from command line argument
    - Command line interface to interact with the application

--- main.go
+++ main.go
@@ -6,8 +6,21 @@ import (
 )
 
 func main() {
-	node, e := NewNode("localhost", "9090")
+	node, e := NewNode(os.Args[1], os.Args[2])
 	if e != nil {
-		os.Exit(1)
+		panic(e)
+	}
+	fmt.Printf("Node running on %s:%s\n", node.IP, node.Port)
+	for {
+		var cmd string
+		fmt.Printf("> ")
+		fmt.Scanf("%s", &cmd)
+		switch cmd {
+		case "get":
+			var key, val string
+			fmt.Scanf("%s", &key)
+			e = node.Get(key, &val)
+			fmt.Printf("%s\n", val)
+		}
 	}
 }
